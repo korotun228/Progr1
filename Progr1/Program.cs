3eb3c0ece9b22a10c902ee63b6ffb5547dbcef65
igor <igor@DESKTOP-97E7NJ7>
2024-04-28 18:47:39 +0200

    Добавьте файлы проекта.

---
+++ Progr1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Progr1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int money, food, foodUnitePrice=10;
+            bool isAbleToPay;
+            Console.WriteLine("dobro pozaovat! segodnia eda prodaetsa po " + foodUnitePrice + " za schtuku");
+            Console.Write("skolko u vas zolota?");
+            money = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("skolko vam nuzno edy? ");
+            food = Convert.ToInt32(Console.ReadLine());
+
+            isAbleToPay = money >= food * foodUnitePrice;
+            food *= Convert.ToInt32(isAbleToPay);
+
+            money -= food * foodUnitePrice;
+            Console.WriteLine($"u vas v sumke {food} edy i {money} deneg ");
+
+           
+            Console.ReadKey();
+        }
+    }
+}
